20ebc654e7407fd978b3f1a8c5cffef120dd31db
redouane <redouaneboukamir@gmail.com>
2018-12-04 12:30:31 +0100

    post

--- data.php
+++ data.php
@@ -1,11 +1,45 @@
 <?php
 
-$firstName = $_POST['first_name'];
-$name = $_POST['last_name'];
-$email = $_POST['email'];
-$genre = $_POST['genre'];
-$pays = $_POST['pays'];
-$sujet = $_POST['autre'];
+ini_set('display_errrors',1);
+session_start();
+if(isset($_POST['first_name']) &&
+isset($_POST['last_name']) && 
+isset($_POST['email']) &&
+!empty($_POST['email']) && 
+isset($_POST['genre']) && 
+($_POST['genre'] == 'homme' || $_POST['genre'] =='femme')&&
+isset($_POST['pays']) && 
+isset($_POST['sujets'])){
 
+    $options = array(
+        'first_name' 	=> FILTER_SANITIZE_STRING,
+        'last_name' 	=> FILTER_SANITIZE_STRING,
+        'genre'         => FILTER_SANITIZE_STRING,  
+        'pays'          =>  FILTER_SANITIZE_STRING, 
+        'email' 		=> FILTER_VALIDATE_EMAIL
+    );
+    // $options += filter_var_array($_POST['sujets'], FILTER_SANITIZE_STRING);
+    foreach($_POST['sujets'] as $value){
+        $options[$value] = FILTER_SANITIZE_STRING;
+    };
+}
+$result = filter_input_array(INPUT_POST, $options);
+ if ($result != null AND $result != FALSE) {
+    $_POST['first_name'] = "";
+    $_POST['last_name'] = "";
+    $_POST['email'] = "";
+    $_POST['genre'] = "";
+    $_POST['pays']= "";
+    $_POST['sujets']="";
+    echo "Merci pour votre inscription";
+    foreach ($result as $key => $value) {
+        var_dump($key. " - " .$result[$key]."<br>");
+    };
+
+} else {
+
+    echo "Un champ est vide ou n'est pas correct!";
+
+}
 
 ?>
--- index.php
+++ index.php
@@ -1,38 +1,6 @@
 <?php
-if(isset($_POST['first_name']) &&
-isset($_POST['last_name']) && 
-isset($_POST['email']) && 
-isset($_POST['genre']) && 
-($_POST['genre'] == 'homme' || $_POST['genre'] =='femme')&&
-isset($_POST['pays']) && 
-isset($_POST['autre'])){
-    $options = array(
-        'first_name' 	=> FILTER_SANITIZE_STRING,
-        'last_Name' 	=> FILTER_SANITIZE_STRING,
-        'genre'         => FILTER_SANITIZE_STRING,  
-        'pays'          =>  FILTER_SANITIZE_STRING, 
-        'email' 		=> FILTER_VALIDATE_EMAIL,
-        'subject' 		=> FILTER_SANITIZE_STRING,
-        'message' 		=> FILTER_SANITIZE_STRING
-    );
-}
-$result = filter_input_array(INPUT_POST, $options);
- if ($result != null AND $result != FALSE) {
-    $_POST['first_name'] = "";
-    $_POST['last_name'] = "";
-    $_POST['email'] = "";
-    $_POST['genre'] = "";
-    $_POST['genre'] = "";
-    $_POST['pays']= "";
-    $_POST['autre']="";
-    echo "Merci pour votre inscription";
-    echo $options['pays'];
+session_start();
 
-} else {
-
-	echo "Un champ est vide ou n'est pas correct!";
-
-}
 ?>
 
 <!DOCTYPE html>
@@ -61,39 +29,39 @@ $result = filter_input_array(INPUT_POST, $options);
         <figure><img src="assets/hackers-poulette-logo.png" alt="LogoHackersPoulette" srcset=""></figure>
     </div>
     <div class="contentForm col 10 col xl6 col l8">
-         <form action="index.php" method="post" class="formulaire row">
+         <form action="data.php" method="post" class="formulaire row">
 
             <div class="row">
                 <div class="input-field col s6">
-                <input placeholder="First Name" id="first_name" name="first_name" type="text" 
+                <input placeholder="First Name" required id="first_name" name="first_name" type="text" 
                 class="validate" alt="Nom" value="<?php echo $_POST['first_name']?>">
                 <label for="first_name">First Name  <i class="fas fa-user-alt"></i></label>
                 </div>
                 <div class="input-field col s6">
-                <input placeholder="Last Name" id="last_name" name='last_name' type="text" 
+                <input placeholder="Last Name" required id="last_name" name='last_name' type="text" 
                 class="validate" alt="Prenom" value="<?php echo $_POST['last_name']?>">
                 <label for="last_name">Last Name  <i class="fas fa-user-alt"></i></label>
                 </div>
             </div>
             <div class="row">
                 <div class="input-field col s12">
-                <input id="email" type="email" name="email" class="validate" alt="Email"
+                <input id="email" type="email" required name="email" class="validate" alt="Email"
                 value="<?php echo $_POST['email']?>">
                 <label for="email">Email  <i class="fas fa-at"></i></label>
                 </div>
             </div>
             <div class="genre">
-            <label>
-                <input id="homme" name="genre" type="radio"  value="homme" alt="Homme"/>
-                <span>Homme</span>
-            </label>
-            <label>
-                <input id="femme" name='genre' type="radio" value="femme"  alt="Femme"/>
-                <span>Femme </span>
-            </label>
+                <label>
+                    <input id="homme" name="genre" type="radio"  value="homme" alt="Homme"/>
+                    <span>Homme</span>
+                </label>
+                <label>
+                    <input id="femme" name='genre' type="radio" value="femme"  alt="Femme"/>
+                    <span>Femme </span>
+                </label>
             </div>
             <div class="input-field col s12">
-                <select name='pays'>
+                <select name='pays' >
                     <option value="pays" disabled selected>Choose your country</option>
                     <option value="Belgique" selected="selected">Belgique </option>
                     <option value="Afghanistan">Afghanistan </option>
@@ -112,17 +80,20 @@ $result = filter_input_array(INPUT_POST, $options);
                 <label>Pays  <i class="fas fa-globe-americas"></i></label>
             </div>
             <div class="input-field col s12">
-            <select name="autre" alt="sujets">
-                <option value="probleme" alt="probleme technique">Problème technique</option>
-                <option value="nouvelle" alt="prendre des nouvelles">Prendre des nouvelles</option>
-                <option value="demande" alt="demande d'information">Demande d'informations</option> 
-            </select>
+                <select multiple name="sujets[]" alt="sujets" >
+                    <option value="" disabled selected>Sujets</option>
+                    <option value="probleme" alt="probleme technique">Problème technique</option>
+                    <option value="nouvelle" alt="prendre des nouvelles">Prendre des nouvelles</option>
+                    <option value="demande" alt="demande d'information">Demande d'informations</option> 
+                    <option value="autres" alt="demande d'information">autres</option> 
+                </select>
                 <label>Sujets  <i class="fas fa-question-circle"></i></label>
             </div>
+            
            <label class="label" alt="Votre message">Votre message : </label>
-            <textarea name="message" id="message" cols="20" rows="15" 
-            value="<?php echo $_POST['message']?>" class="materialize-textarea"></textarea>
-            <input class="submit col s4 push-s4" id='submit' type="submit">
+            <textarea name="message" id="message" cols="20" rows="15" class="materialize-textarea"></textarea>
+            <input class="submit col s4 push-s4" id='submit' type="submit"  
+            <?php     ?> >
 
         </form>
     </div> 
